1383b04a70c7f54c0479ed0080939a836e153c4d
John Sullivan <jsullivan@csumb.edu>
2017-12-03 23:13:24 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Read into the mind of @realDonaldTrump
---
+++ trumble.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import zipfile
+import urllib.request
+import html
+import json
+import re
+import numpy as np
+
+URL_F = 'https://github.com/bpb27/trump_tweet_data_archive/raw/master/condensed_{}.json.zip'
+TWEET_YEARS = ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
+
+regex = re.compile(r'[\r\n\t]')
+
+def fix_tweets(fnames):
+    tweets = []
+    for fname in fnames:
+        with open(fname) as fp:
+            for msg in json.load(fp):
+                if msg['is_retweet']:
+                    continue
+                text = regex.sub('', html.unescape(msg['text']))
+                tweets.append(text)
+    return np.array(tweets)
+
+def collect_tweets(download = True, years = TWEET_YEARS, dl_years = TWEET_YEARS):
+    if download:
+        for year in dl_years:
+            request = urllib.request.urlopen(URL_F.format(year))
+            with open(f'condensed_{year}.json.zip', 'wb') as fp:
+                fp.write(request.read())
+            with zipfile.ZipFile(f'condensed_{year}.json.zip') as jzip:
+                jzip.extractall()
+    return fix_tweets([f'condensed_{year}.json' for year in years])
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description = 'Download Trump Tweets')
+    parser.add_argument('years', metavar = 'YYYY', type = str, nargs = '*', default = TWEET_YEARS,
+                        help = 'Years to collect, valid from 2009 (default: all years)')
+    parser.add_argument('--nodl', dest = 'use_download', action = 'store_const', const = False, default = True,
+                        help = 'Set this to disable downloading (default: download new tweets from archive)')
+    args = parser.parse_args()
+
+    tweets = collect_tweets(download = args.use_download, years = args.years, dl_years = args.years)
+    for tweet in tweets:
+        print(tweet)
